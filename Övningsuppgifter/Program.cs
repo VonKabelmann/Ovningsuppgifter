ce5eee01048481b2d494e42a9e30f7363bc53589
Pontus Ekdahl <pontus.ekdahl@gmail.com>
2022-09-01 14:32:37 +0200

    Uppgift 7 v0.5

--- Övningsuppgifter/Program.cs
+++ Övningsuppgifter/Program.cs
@@ -86,67 +86,152 @@
 
 //Övningsuppgift 6
 
-int firstNum;
-int secondNum;
-char op;
+//int firstNum;
+//int secondNum;
+//char op;
 
 
+//while (true)
+//{
+//    Console.Write("Enter your first number: ");
+//    if (!Int32.TryParse(Console.ReadLine(), out firstNum))
+//    {
+//        Console.WriteLine("Invalid input.");
+//    }
+//    else
+//    {
+//        break;
+//    }
+//}
+//while (true)
+//{
+//    Console.Write("Please enter an operator ('/', '*', '+' '-'): ");
+//    if (Char.TryParse(Console.ReadLine(), out op))
+//    {
+//        if (op == '/' || op == '*' || op == '+' || op == '-')
+//        {
+//            break;
+//        }
+//        else
+//        {
+//            Console.WriteLine("Invalid operator!");
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine("Invalid operator!");
+//    }
+//}
+//while (true)
+//{
+//    Console.Write("Enter your second number: ");
+//    if (!Int32.TryParse(Console.ReadLine(), out secondNum))
+//    {
+//        Console.WriteLine("Invalid input.");
+//    }
+//    else
+//    {
+//        break;
+//    }
+//}
+//if (op == '/')
+//{
+//    Console.WriteLine($"{firstNum} divided by {secondNum} is equal to {Convert.ToDouble(Convert.ToDouble(firstNum) / Convert.ToDouble(secondNum))}");
+//}
+//else if (op == '*')
+//{
+//    Console.WriteLine($"{firstNum} multiplied by {secondNum} is equal to {firstNum * secondNum}");
+//}
+//else if (op == '+')
+//{
+//    Console.WriteLine($"{firstNum} plus {secondNum} is equal to {firstNum + secondNum}");
+//}
+//else if (op == '-')
+//{
+//    Console.WriteLine($"{firstNum} minus {secondNum} is equal to {firstNum - secondNum}");
+//}
+
+//Övningsuppgift 7
+
+//Skriv ett program som ber användaren skriva in en månad. Programmet ska göra om månaden till månadens siffervärde. 
+//    T.ex. ska januari bli 1 och december 12. Använd switch. Alternativ: Prova även att lösa uppgiften med array och/eller enum.
+
+//Extra utmaning: Be användaren om två månader och räkna sedan ut hur många månader som skiljer mellan dem. 
+//    Tänk på att ett månaderna upprepas till nästa år. Skriver man mars och maj bör man få 2. 
+//    Likaså om man skriver november och januari.
+
+string[] months = { "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" };
+string monthInput1;
+string monthInput2;
+int monthIndex1;
+int monthIndex2;
+
+Console.Write("Input a month: ");
 while (true)
 {
-    Console.Write("Enter your first number: ");
-    if (!Int32.TryParse(Console.ReadLine(), out firstNum))
+    monthInput1 = Console.ReadLine().ToLower();
+	if (Array.IndexOf(months, monthInput1) == -1)
+	{
+		Console.WriteLine("There is no such month.");
+		Console.Write("Please enter a valid month: ");
+	}
+	else
+	{
+        monthIndex1 = Array.IndexOf(months, monthInput1);
+        break;
+	}
+}
+Console.WriteLine("Input a second month: ");
+while (true)
+{
+    monthInput2 = Console.ReadLine().ToLower();
+    if (Array.IndexOf(months, monthInput2) == -1)
     {
-        Console.WriteLine("Invalid input.");
+        Console.WriteLine("There is no such month.");
+        Console.Write("Please enter a valid month: ");
     }
     else
     {
+        monthIndex2 = Array.IndexOf(months, monthInput1);
         break;
     }
 }
-while (true)
+switch (monthIndex1)
 {
-    Console.Write("Please enter an operator ('/', '*', '+' '-'): ");
-    if (Char.TryParse(Console.ReadLine(), out op))
-    {
-        if (op == '/' || op == '*' || op == '+' || op == '-')
-        {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Invalid operator!");
-        }
-    }
-    else
-    {
-        Console.WriteLine("Invalid operator!");
-    }
-}
-while (true)
-{
-    Console.Write("Enter your second number: ");
-    if (!Int32.TryParse(Console.ReadLine(), out secondNum))
-    {
-        Console.WriteLine("Invalid input.");
-    }
-    else
-    {
+    case 0:
+        
         break;
-    }
-}
-if (op == '/')
-{
-    Console.WriteLine($"{firstNum} divided by {secondNum} is equal to {Convert.ToDouble(Convert.ToDouble(firstNum) / Convert.ToDouble(secondNum))}");
-}
-else if (op == '*')
-{
-    Console.WriteLine($"{firstNum} multiplied by {secondNum} is equal to {firstNum * secondNum}");
-}
-else if (op == '+')
-{
-    Console.WriteLine($"{firstNum} plus {secondNum} is equal to {firstNum + secondNum}");
-}
-else if (op == '-')
-{
-    Console.WriteLine($"{firstNum} minus {secondNum} is equal to {firstNum - secondNum}");
+    case 1:
+        
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    case 5:
+
+        break;
+    case 6:
+
+        break;
+    case 7:
+
+        break;
+    case 8:
+
+        break;
+    case 9:
+
+        break;
+    case 10:
+
+        break;
+    case 11:
+
+        break;
 }
